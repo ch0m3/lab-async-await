2ebcda2b304b65f3cb9d958461e4a70ff4c4505e
Chome <chomeallan@gmail.com>
2026-01-09 18:30:03 +0300

    imenipita

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your code here!
+async function fetchPosts() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+    displayPosts(posts);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function displayPosts(posts) {
+  const ul = document.getElementById("post-list");
+  if (!ul) return;
+
+  posts.forEach(post => {
+    const li = document.createElement("li");
+
+    const h1 = document.createElement("h1");
+    h1.textContent = post.title;
+
+    const p = document.createElement("p");
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    ul.appendChild(li);
+  });
+}
+
+//fetchPosts();
+document.addEventListener("DOMContentLoaded", () => {
+  fetchPosts();
+});
